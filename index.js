f9ca3355ab3be742038c08a957fc790d7f437f1e
xapho <51489624+xapho@users.noreply.github.com>
2024-05-17 23:50:03 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-console.log("hola mundo")
+const express = require('express');
+const app = express();
+const connectDB = require('./src/config/db.config');
+const teamRoute = require('./src/routes/Team.route.js')
+const torneoRoute = require('./src/routes/Torneo.route.js')
+const partidoRoute = require('./src/routes/Partido.route')
+const cors = require('cors');
+
+// Configurar CORS para permitir solicitudes solo desde http://localhost:5173
+app.use(cors({
+  origin: 'http://localhost:5173'
+}));
+
+//esto va en el middleware recordar
+app.use(express.json());
+
+//rutas del programa
+app.use('/api/teams', teamRoute);
+app.use('/api/torneos', torneoRoute);
+app.use('/api/partidos', partidoRoute)
+
+app.listen(3000, () => {
+  console.log('el server se encuentra en el local 3000');
+});
+
+//conexion con la base de datos de index.js
+connectDB();
+
+
